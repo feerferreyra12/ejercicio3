89e8df5e7961698b82d54569c7e6aa23791070bc
feerferreyra12 <123518779+feerferreyra12@users.noreply.github.com>
2023-02-23 19:24:09 -0300

    Add files via upload
    función con tres parámetros que sean números que se suman entre sí

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static int sumarTresNumeros(int a, int b, int c){
+        int suma = a + b + c;
+        return suma;
+    }
+    public static void main(String[] args) {
+        int resultado = sumarTresNumeros(5,10 ,15 );
+        System.out.println(resultado);
+    }
+}
+
